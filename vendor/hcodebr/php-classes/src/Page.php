b0192aa0d9897f9a15963bb8e519a02b9a0895f2
Fábio <fabioalmeida132@gmail.com>
2019-01-02 00:18:12 -0200

    02.11

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,13 +8,15 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
-    public function __construct($opts = array())
+    public function __construct($opts = array(), $tpl_dir = "/views/")
     {
         $this->options = array_merge($this->defaults, $opts);
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views/cache/",
             "debug"         => false // set to false to improve the speed
         );
@@ -25,7 +27,7 @@ class Page{
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -44,7 +46,7 @@ class Page{
 
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+         if($this->options["footer"] === true )$this->tpl->draw("footer");
     }
 }
 
